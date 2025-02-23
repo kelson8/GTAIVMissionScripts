326f86a19c24b747edf055a743aad864803bcfc9
kelson8 <kelson@kelsoncraft.net>
2025-02-22 20:47:16 -0500

    Rename all files from .txt to .c, add some documentation to initial.c under sub_33971.

---
+++ IV/test/Readme.md
@@ -0,0 +1,3 @@
+# Look into
+initial.c:
+* void sub_33971() - This contains some vehicle generators, helipads and locations that are in the water for some reason.
---
+++ rename-files.bat
@@ -0,0 +1,5 @@
+:: https://stackoverflow.com/questions/12120326/how-do-i-change-the-extension-of-many-files-in-a-directory
+
+:: This was used to rename all .txt files to .c, since these are just C compatible files.
+
+ren *.txt *.c
